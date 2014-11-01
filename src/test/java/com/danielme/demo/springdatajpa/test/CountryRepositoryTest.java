5cf2d240c843bd1c4f08a61b8e687b9097bdfe64
danielme.com <danielme_com@yahoo.com>
2014-11-01 22:04:52 +0100

    JUNit instead of Main class

---
+++ src/test/java/com/danielme/demo/springdatajpa/test/CountryRepositoryTest.java
@@ -0,0 +1,102 @@
+package com.danielme.demo.springdatajpa.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Calendar;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.danielme.demo.springdatajpa.model.Country;
+import com.danielme.demo.springdatajpa.repository.CountryRepository;
+import com.danielme.demo.springdatajpa.repository.specifications.CountrySpecifications;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/resources/applicationContext.xml")
+public class CountryRepositoryTest
+{
+
+	@Autowired
+	private CountryRepository countryRepository;
+
+	private static boolean springInit = false;
+
+	@Before
+	@Transactional
+	public void setUp() throws Exception
+	{
+		if (!springInit)
+		{
+			// empty repository
+			countryRepository.deleteAllInBatch();
+
+			// insert
+			countryRepository.save(new Country("Spain", 47265321));
+			countryRepository.save(new Country("Mexico", 115296767));
+			countryRepository.save(new Country("Germany", 81799600));
+			countryRepository.save(new Country("Finland", 5470820));
+			countryRepository.save(new Country("Colombia", 47846160));
+			countryRepository.save(new Country("Costa Rica", 4586353));
+			countryRepository.save(new Country("Norway", 5136700));
+			springInit = true;
+		}
+	}
+
+	@Test
+	public void testSimpleQuerys()
+	{
+		assertTrue(countryRepository.findByName("Germany").getName().equals("Germany"));
+		assertNull(countryRepository.findByName("France"));
+		assertTrue(countryRepository.countByPopulationGreaterThan(45000000) == 4);
+		assertTrue(countryRepository.findByPopulationGreaterThan(100000000).get(0).getName().equals("Mexico"));
+		assertTrue(countryRepository.exists("Spain"));
+		assertTrue(countryRepository.getByPopulationNamedQuery(5470820).getName().equals("Finland"));
+		assertFalse(countryRepository.exists("Italy"));
+
+	}
+
+	@Test
+	public void testQuerysSortingAndPaging()
+	{
+		List<Country> countries = countryRepository.findByPopulationGreaterThanOrderByPopulationAsc(45000000);
+		assertTrue(countries.size() == 4);
+		assertTrue(countries.get(0).getName().equals("Spain"));
+		assertTrue(countries.get(3).getName().equals("Mexico"));
+
+		Page<Country> page0 = countryRepository.getByNameWithQuery("%i%", new PageRequest(0, 4, new Sort(
+				new Sort.Order(Sort.Direction.ASC, "name"))));
+		assertTrue(page0.getTotalElements() == 5);
+		assertTrue(page0.getTotalPages() == 2);
+		assertTrue(page0.isFirst());
+		assertTrue(page0.getContent().get(0).getName().equals("Colombia"));
+
+	}
+
+	@Test
+	public void testModifyingQuerys() throws Exception
+	{
+		Calendar creation = countryRepository.findByName("Norway").getCreation();
+		Thread.sleep(2000);
+		assertTrue(countryRepository.updateCreation(Calendar.getInstance()) == 7);
+		assertTrue(countryRepository.findByName("Norway").getCreation().after(creation));
+	}
+
+	@Test
+	public void testJpaCriteria()
+	{
+		assertTrue(countryRepository.findOne(CountrySpecifications.searchByName("Mexico")).getName().equals("Mexico"));
+	}
+
+}
